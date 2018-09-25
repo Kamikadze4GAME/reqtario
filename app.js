2440bee063784c2df95b28e278065ca3e95a74be
Kamikadze4GAME <osskrisha@gmail.com>
2018-09-25 23:34:54 +0300

    Fixes admins

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(logger('dev'));
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+if(process.env.UPLOADS_DIR) {
+  app.use('/uploads', express.static(process.env.UPLOADS_DIR));
+}
 app.use(expressMongoDb('mongodb://localhost/reqtario'));
 
 
